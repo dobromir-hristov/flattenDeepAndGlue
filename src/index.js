d44addc5ea487f026e87b67cc96458daa1d83959
Dobromir Hristov <djdobro@abv.bg>
2017-10-02 23:59:29 +0300

    chore: Add Jest and more tests

--- src/index.js
+++ src/index.js
@@ -1,27 +1,25 @@
 /**
  * Transform an array of messages from laravel error message bag to be a single html string with breaks
  * @param {Object | Array | String} message
- * @param {String} $glue
+ * @param {String | Boolean} $glue
  * @example
  * message = { username: ['some error', 'maybe another error'], email: ['another error', 'and another error']}
  * message = { username: 'some error', email: 'and another error'}
  * message = ['some error', 'and another error']
  * message = [['some error'], ['and another error']]
  * message = 'some error'
- * @return { String }
+ * @return { String | Array }
  */
 export default function transformMessage (message, $glue = '<br/>') {
-  // Check if messages are an Object of array items
+  // Check if messages are an Object. Arrays also count here
   if (typeof message === 'object') {
-    return Object.keys(message).reduce((msg, key) => {
+    const messageArray = Object.keys(message).reduce((msg, key) => {
       // We flatten the message if its an obj or just pass it if is a string.
-      if (typeof message[key] === 'object') {
-        msg.push(...message[key])
-      } else {
-        msg.push(message[key])
-      }
-      return msg
-    },[]).join($glue)
+      let transformed = transformMessage(message[key], $glue)
+      typeof transformed === 'object' && (transformed = [...transformed])
+      return msg.concat(transformed)
+    }, [])
+    return $glue ? messageArray.join($glue) : messageArray
   }
   // its not an object so we just return it.
   return message
--- tests/test.js
+++ tests/test.js
@@ -1,42 +1,74 @@
-import test from 'ava'
-import transformMessage from '../dist/objectToBrSeparatedString'
+import transformMessage from '../src'
 
-test('Component should be loaded', t => {
-  t.is(typeof transformMessage, 'function', 'Imports the function properly')
+test('Component should be loaded', () => {
+  expect(typeof transformMessage).toBe('function')
 })
 
-test('Should leave strings alone', t => {
+test('Should return strings when providing strings', () => {
   const message = 'Name is required',
     expected = 'Name is required'
-  t.is(transformMessage(message), expected)
+  expect(transformMessage(message)).toBe(expected)
 })
 
-test('Should transform objects with keys and strings', t => {
-  const message = {name: 'Name is required', email: 'Email is required'},
+test('Should transform objects strings as values', () => {
+  const message = { name: 'Name is required', email: 'Email is required' },
     expected = 'Name is required<br/>Email is required'
-  t.is(transformMessage(message), expected)
+  expect(transformMessage(message)).toBe(expected)
 })
 
-test('Should transform objects with keys and arrays of strings', t => {
-  const message = {name: 'Name is required', email: ['Email is required', 'Email is not valid']},
+test('Should transform objects with arrays of strings', () => {
+  const message = {
+      name: 'Name is required',
+      email: ['Email is required', 'Email is not valid']
+    },
     expected = 'Name is required<br/>Email is required<br/>Email is not valid'
-  t.is(transformMessage(message), expected)
+  expect(transformMessage(message)).toBe(expected)
 })
 
-test('Should transform array of strings', t => {
-  const message = ['Name is required','Email is required', 'Email is not valid'],
+test('Should transform array of strings', () => {
+  const message = ['Name is required', 'Email is required', 'Email is not valid'],
     expected = 'Name is required<br/>Email is required<br/>Email is not valid'
-  t.is(transformMessage(message), expected)
+  expect(transformMessage(message)).toBe(expected)
 })
 
-test('Should transform array of arrays of strings', t => {
-  const message = [['Name is required','Email is required'], 'Email is not valid'],
+test('Should transform array of arrays of strings', () => {
+  const message = [['Name is required', 'Email is required'], 'Email is not valid'],
     expected = 'Name is required<br/>Email is required<br/>Email is not valid'
-  t.is(transformMessage(message), expected)
+  expect(transformMessage(message)).toBe(expected)
 })
 
-test('Should transform object of strings to comma separated list', t => {
-  const message = {name: 'Name is required', email: 'Email is required'},
+test('Should transform object of strings to comma separated list', () => {
+  const message = { name: 'Name is required', email: 'Email is required' },
     expected = 'Name is required, Email is required'
-  t.is(transformMessage(message, ', '), expected)
+  expect(transformMessage(message, ', ')).toBe(expected)
+})
+
+test('Should transform object of nested objects', () => {
+  const message = { name: 'Name is required', email: { valid: 'Not Valid' } },
+    expected = 'Name is required<br/>Not Valid'
+  expect(transformMessage(message)).toBe(expected)
+})
+
+test('Should transform object of nested objects', () => {
+  const message = { name: 'Name is required', email: { fields: ['Invalid', 'Required'] } },
+    expected = 'Name is required<br/>Invalid<br/>Required'
+  expect(transformMessage(message)).toBe(expected)
+})
+
+test('Should return flat array of items', () => {
+  const message = { name: 'Required', email: ['Not Valid', 'Required'] },
+    expected = ['Required', 'Not Valid', 'Required']
+  expect(transformMessage(message, false)).toEqual(expected)
+})
+
+test('Should transform object of deeply nested objects to flat array', () => {
+  const message = { name: 'Required', email: { fields: ['Not Valid', 'Required'] } },
+    expected = ['Required', 'Not Valid', 'Required']
+  expect(transformMessage(message, false)).toEqual(expected)
+})
+
+test('Should transform mixed array of numbers, objects, strings', () => {
+  const message = [5, 'error', { name: 'Required' }],
+    expected = '5,error,Required'
+  expect(transformMessage(message, ',')).toBe(expected)
 })
